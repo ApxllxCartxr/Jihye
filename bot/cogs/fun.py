6a647b6a19d248745a856b14aaa56e8a7f3ede49
Joseph Fernando <77870424+ApxllxCartxr@users.noreply.github.com>
2022-09-03 06:35:11 +0000

    uh

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -3,16 +3,20 @@ from io import BytesIO
 from urllib.request import urlopen
 import textwrap
 import fast_colorthief 
-import wcag_contrast_ratio as contrast
-
-
-
-
 
 import disnake
 from disnake.ext import commands
 
-
+async def contrast(bg, color):
+    r1,g1,b1 = bg
+    r2,g2,b2 = color
+    r1,g1,b1 = (r1/255)^2.2,(g1/255)^2.2,(b1/255)^2.2
+    r2,g2,b2 = (r2/255)^2.2,(g2/255)^2.2,(b2/255)^2.2
+    Y1 = 0.2126*r1 + 0.7151*g1 + 0.0721*b1
+    Y2 = 0.2126*r2 + 0.7151*g2 + 0.0721*b2
+    cont = (Y1+0.05/Y2+0.05) if Y1>Y2 else (Y2+0.05/Y1+0.05)
+    print(cont)
+    return cont
 
 
 
@@ -51,41 +55,43 @@ class fun(commands.Cog):
                 song = textwrap.shorten(activity.title, width=20, placeholder="...")
                 artists = textwrap.shorten(', '.join(activity.artists), width=35, placeholder="...")
                 for i, color in enumerate(colors):
-                    cont = contrast.rgb(bg, color)
-                    if contrast.passes_AAA(cont):
-                        im = Image.new(mode="RGB", size=(1000, 300), color=bg)
-                        draw = ImageDraw.Draw(im=im)
-    
-                        draw.text(
-                            xy=(280, 110),
-                            text=f"{activity.album}",
-                            font=thin,
-                            fill=color,
-                            anchor="ls",
-                        )
-                        draw.text(
-                            xy=(280, 170),
-                            text=f"{song}",
-                            font=regular,
-                            fill=color,
-                            anchor="ls",
-                        )
-                        draw.text(
-                            xy=(280,210),
-                            text=f"by {artists}",
-                            font=thin,
-                            fill=color,
-                            anchor="ls",
-                        )
-                        Image.Image.paste(im, img, (30, 40))           
-                        with BytesIO() as image_binary:
-                            im.save(image_binary, "PNG")
-                            image_binary.seek(0)
-                            await ctx.send(
-                                file=disnake.File(fp=image_binary, filename="image.png")
-                            )
-                    else:
-                        continue
-
+                    cont = contrast(bg, color)
+                    print(cont)
+                    
 def setup(bot):
     bot.add_cog(fun(bot))
+# if cont>=4:
+#                         print(f"contrast = {cont}")
+#                         im = Image.new(mode="RGB", size=(1000, 300), color=bg)
+#                         draw = ImageDraw.Draw(im=im)
+    
+#                         draw.text(
+#                             xy=(280, 110),
+#                             text=f"{activity.album}",
+#                             font=thin,
+#                             fill=color,
+#                             anchor="ls",
+#                         )
+#                         draw.text(
+#                             xy=(280, 170),
+#                             text=f"{song}",
+#                             font=regular,
+#                             fill=color,
+#                             anchor="ls",
+#                         )
+#                         draw.text(
+#                             xy=(280,210),
+#                             text=f"by {artists}",
+#                             font=thin,
+#                             fill=color,
+#                             anchor="ls",
+#                         )
+#                         Image.Image.paste(im, img, (30, 40))           
+#                         with BytesIO() as image_binary:
+#                             im.save(image_binary, "PNG")
+#                             image_binary.seek(0)
+#                             await ctx.send(
+#                                 file=disnake.File(fp=image_binary, filename="image.png")
+#                             )
+#                     else:
+#                         print("NO?")
